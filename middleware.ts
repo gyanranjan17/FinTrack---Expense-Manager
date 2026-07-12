b79185dac7d50aaea605fdd72ea6b51bd9deb388
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2026-07-12 13:53:37 +0530

    Refactor middleware for authentication handling

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,24 @@
-import { clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server';
-import arcjet, {createMiddleware, detectBot, shield} from "@arcjet/next";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
   "/transaction(.*)",
-])
+]);
 
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  rules:[
-    shield({
-      mode:"LIVE",
-    }),
-    detectBot({
-      mode:"LIVE",
-      allow:[
-        "CATEGORY:SEARCH_ENGINE","GO_HTTP"
-      ],
-    })
-  ]
-});
-
-const clerk =  clerkMiddleware( async(auth,req) => {
-  const {userId, redirectToSignIn} = await auth()
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
-    // const {redirectToSignIn} = await auth();  
     return redirectToSignIn();
   }
-
 });
-
-export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Skip Next.js internals and static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
